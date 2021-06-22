6d3f9a05507503da269c013c503ef9ad261c2fa9
danibondar <danibondar@gmail.com>
2021-06-22 10:15:38 +0300

    malloc_1.cpp

--- malloc_1.cpp
+++ malloc_1.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
+#include <unistd.h>
+#define MAX_SIZE 100000000
+void *smalloc(size_t size) {
+    if (size == 0 || size > MAX_SIZE) {
+        return nullptr;
+    }
+    void *old_brk = sbrk(0);
+    if (old_brk == (void *) -1) {
+        return nullptr;
+    }
+    sbrk(size);
 
-void* smalloc(size_t size){
-    return nullptr;
+    return old_brk;
 }
-
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
